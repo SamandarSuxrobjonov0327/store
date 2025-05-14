01c76d786510fcfe790efeb90ca789bd277c802e
Samandar Suhrobjonov <macbook1@MacBook-Air-macbook.local>
2025-05-14 15:58:31 +0500

    hammaga yaratildi

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar';
 import Content from '../../components/Content';
+import { Link } from 'react-router-dom';
 
 export default () => (
   <div>
-    <Sidebar />
-    <Content />
+    <div><Link to="ulugbek">ulugbek</Link></div>
+    <div>
+    <Link to="abdurashid">abdurashid</Link>
+    </div>
+    <div>
+    <Link to="nozimjon">nozimjon</Link>
+    </div>
+    <div>
+    <Link to="narimon">narimon</Link>
+    </div>
+    <Link to="donyor">donyor</Link>
   </div>
 );
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import Users from '../views/Users/List';
 import Home from '../containers/Home';
+import Ulugbek from "../containers/ulugbek/mainHome"
+import Abdurashid from "../containers/abdurashid/index"
+import Nozimjon from "../containers/nozimjon/index"
+import Narimon from "../containers/narimon/narimon"
+import Donyor from "../containers/donyor/index"
 
 export const routes = [
   {
     // element: <Home />,
     children: [
       { index: true, element: <Home /> },
-      { path: '/users', element: <Users /> },
-      { path: '*', element: <Navigate to='/' /> },
+      { path: "/ulugbek", element: < Ulugbek/> },
+      { path: "/abdurashid", element: < Abdurashid/> },
+      { path: "/nozimjon", element: < Nozimjon/> },
+      { path: "/narimon", element: < Narimon/> },
+      { path: "/donyor", element: < Donyor/> },
     ],
   },
 ];
