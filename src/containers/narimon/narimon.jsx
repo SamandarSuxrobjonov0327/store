ded17afe88796aeee023a81626bffd87df5ed419
narimon92 <narimonmarufjonov@gmail.com>
2025-05-14 16:45:08 +0500

    men qoshildim

--- src/containers/narimon/narimon.jsx
+++ src/containers/narimon/narimon.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <div>
+      <h1>men narimon</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus culpa dolore ullam quo. Dolor impedit consequatur provident, expedita doloribus voluptatibus magnam voluptatem temporibus animi quis tempora, exercitationem velit enim eum?</p>
+    </div>
   )
 }
 
