82833170bf86a6dad8507f611addc80d547c0810
abdimaster <dasturchimaster2011@gmail.com>
2025-05-14 16:35:25 +0500

    qoshdim

--- src/containers/abdurashid/index.jsx
+++ src/containers/abdurashid/index.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <div>
+      <h1>men abdurashid</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio sit iusto, pariatur quaerat non corporis nemo iste assumenda possimus vitae sequi magni repellat quam neque? Adipisci voluptate nostrum consequuntur unde.
+      </p>
+      <span>jgvbnsjdfbn</span>
+    </div>
   )
 }
 
